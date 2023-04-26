9ec345c0b722a8d846970fbf523c30b9b3f07039
aibbbusr <53560342+aibbbusr@users.noreply.github.com>
2023-04-26 16:43:47 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -2,7 +2,7 @@ module.exports = {
   appid: "wxc725eed80d9a57d9",
   appsecret: "efd60ffe8e91466083e5ed6b66aa8c17",
   template_id: "v4vytB5ozHT5DTFLlRmqUgEfdvsqglEOwe2axYTpCNU",
-  touser: "oonWJ5slWuS2OrJdgCLM3GmNdxVk",
+  touser: "oac026FL8kSWrNvFdlAu69hlVaCs",
 
   key: "201c93c2e2b34bdeecdbf9f5265eb84e",
   city: "成都",
